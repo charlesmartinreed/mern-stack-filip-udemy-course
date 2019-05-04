e114f652582e6052a8e9acc65ccbfa4d368119e5
charlesmartinreed <charlesmartinreed@icloud.com>
2019-05-04 18:05:47 -0500

    Setup basic babel transpiling.

---
+++ dist/js/main.js
@@ -0,0 +1,14 @@
+"use strict";
+
+var x = {
+  a: 1,
+  b: 2,
+  c: 3
+};
+console.log(Object.entries(x));
+var y = {
+  d: 4,
+  e: 5,
+  f: 6
+};
+console.log(Object.entries(y));
---
+++ src/js/main.js
@@ -0,0 +1,5 @@
+const x = { a: 1, b: 2, c: 3 };
+console.log(Object.entries(x));
+
+const y = { d: 4, e: 5, f: 6 };
+console.log(Object.entries(y));
